54215769032ed210d528db1969f6b8020bb1fe28
sathumaimoamo <99487830+satsumaimoamo@users.noreply.github.com>
2022-04-24 01:41:16 +0900

    薩摩の春のエラー祭り

---
+++ SuperNewRoles/Roles/BlueTooth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
+using Hazel;
+using SuperNewRoles.Helpers;
+using SuperNewRoles.Patch;
+using UnityEngine;
+
+namespace SuperNewRoles.Roles
+{
+    class BlueTooth
+    {
+        CustomRPC.PlaySoundRPC(__instance(この__instanceはPlayerControl __instance).PlayerId,Sounds.KillSound);
+    }  
+} 
